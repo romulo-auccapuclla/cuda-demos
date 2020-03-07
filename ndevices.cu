44407e96168bff5aa6e9955943016acd3c3a6d93
Mario Inga <mario21ic@gmail.com>
2020-03-06 19:10:22 -0500

    Ready

--- ndevices.cu
+++ ndevices.cu
@@ -10,6 +10,11 @@ int main() {
     printf("Device Number: %d\n", i);
     printf("  Device name: %s\n", prop.name);
     printf("  Compute Capability: %d.%d\n", prop.major, prop.minor);
+    printf("  MultiProccessor: %d\n", prop.multiProcessorCount);
+    printf("  maxThreadsPerMultiProcessor: %d\n", prop.maxThreadsPerMultiProcessor);
+    printf("  regsPerMultiprocessor: %d\n", prop.regsPerMultiprocessor);
+    printf("  Total Memory mb: %zu\n", prop.totalGlobalMem/(1024*1024));
+    printf("  sharedMemPerMultiprocessor: %zu\n", prop.sharedMemPerMultiprocessor);
     printf("  Memory Clock Rate (KHz): %d\n", prop.memoryClockRate);
     printf("  Memory Bus Width (bits): %d\n", prop.memoryBusWidth);
     printf("  Peak Memory Bandwidth (GB/s): %f\n\n", 2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6);
