28ef4a5099f8eec382772ad738785820900bbb0d
Mario Inga <mario21ic@gmail.com>
2020-03-09 11:54:30 -0500

    adding version

--- ndevices/ndevices.cu
+++ ndevices/ndevices.cu
@@ -1,6 +1,14 @@
+#include <cuda.h>
 #include <stdio.h> 
 
 int main() {
+  int driver_version = 0, runtime_version = 0;
+  cudaDriverGetVersion(&driver_version);
+  cudaRuntimeGetVersion(&runtime_version);
+
+  printf("Driver Version: %d\n Runtime Version: %d\n", \
+    driver_version, runtime_version);
+
   int nDevices;
 
   cudaGetDeviceCount(&nDevices);
